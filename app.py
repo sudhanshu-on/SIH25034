be05a8b0d224afccbf4da674b2c04aaaa3e1d63f
Sudhanshu Bagde <sudhanshubagde1234@gmail.com>
2025-09-09 18:44:28 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import requests
-
+from bs4 import BeautifulSoup
 # --- File paths ---
 STUDENT_FILE = "students.csv"
 INTERNSHIP_FILE = "internships.csv"
